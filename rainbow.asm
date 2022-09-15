49ddfe916394de34d7cae53b706f5f589b9d8a03
PastaPad <104093664+PastaPad@users.noreply.github.com>
2022-09-15 17:23:34 +0200

    static rainbow
    an static rainbow like white-black interuppts in TV but in rainbow

---
+++ rainbow.asm
@@ -0,0 +1,22 @@
+bits 16
+org 0x7c00
+mov bx, 0xa000
+mov es, bx
+mov ax, 0x13
+int 0x10
+v1:
+	xor ax, si
+	inc cl
+v2:
+    jge elp
+    inc al
+elp:
+    add dx, si
+    stosb
+    inc dx
+	jnz v1
+	inc si
+    jnz v2
+
+times 510-$+$$ db 0
+dw 0xAA55
