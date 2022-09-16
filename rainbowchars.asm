c377c585038c2dc32ec6a0fba36212ee14699f62
PastaPad <104093664+PastaPad@users.noreply.github.com>
2022-09-16 18:09:37 +0200

    deleted crap

--- rainbowchars.asm
+++ rainbowchars.asm
@@ -4,24 +4,16 @@ mov bx, 0xa000
 mov es, bx
 mov ax, 0x13
 int 0x10
-v1:
-    call print
-print:								
-    push ax
-    cld
 v2:
     jge elp
     inc al
 elp:
     call printchar
-    add dx, si
     stosb
-    inc dx
+    dec dx
     jnz v2
     inc bl
     jnz v2
-    inc di
-    jmp v1
 
 printchar:							
     mov ah,0x0E
